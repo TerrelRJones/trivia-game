b0a22cfc05c92086df6d841db61b538eaa83c765
Terrel Jones <terrel.jones@bounteous.com>
2022-08-05 13:07:03 -0700

    created mockData

---
+++ src/components/QuestionDialog/mockQuestionData.ts
@@ -0,0 +1,6 @@
+export const questionOne = {
+  question:
+    'Which best selling toy of 1983 caused hysteria, resulting in riots breaking out in stores?',
+  options: ['Transformers', 'Cabbage Patch Kids', "Rubik's Cube", 'Care Bears'],
+  answer: 'Cabbage Patch kids',
+};
