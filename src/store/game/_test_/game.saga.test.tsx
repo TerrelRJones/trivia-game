e14ea8cac8c9dfb1d3dd895aaa2697ae34aac1b8
Terrel Jones <terrel.jones@bounteous.com>
2022-08-11 13:15:07 -0700

    added try catch, and updated test

--- src/store/game/_test_/game.saga.test.tsx
+++ src/store/game/_test_/game.saga.test.tsx
@@ -1,11 +1,15 @@
 import { AttackStrengthType } from 'models';
 import { getQuestionFetch } from '../game.saga';
-import { getQuestion } from '../game.slice';
-import { call } from 'redux-saga/effects';
+import { getQuestion, getQuestionSuccess } from '../game.slice';
+import { call, put } from 'redux-saga/effects';
 import { BASE_QUESTION_URL } from 'const/Urls';
+import { QuestionStatus } from 'models';
+import * as ShuffleHelper from 'helper/shuffleArray';
 
 describe('getQuestionsFetchSaga', () => {
   it('should call api to fetch hard question, and store in redux on success.', () => {
+    const shuffleArraySpy = jest.spyOn(ShuffleHelper, 'shuffleArray');
+
     const action = getQuestion({ difficulty: AttackStrengthType.HARD });
     const iterator = getQuestionFetch(action);
 
@@ -37,5 +41,43 @@ describe('getQuestionsFetchSaga', () => {
     };
 
     iterator.next(mockResponse);
+
+    const { question, correct_answer, incorrect_answers } = mockData.results[0];
+    const mockChoices = [correct_answer, ...incorrect_answers];
+    shuffleArraySpy.mockReturnValue(mockChoices);
+
+    const expectedQuestion = {
+      status: QuestionStatus.IDLE,
+      text: question,
+      answer: correct_answer,
+      choices: mockChoices,
+    };
+
+    expect(iterator.next(mockData).value).toEqual(
+      put(getQuestionSuccess(expectedQuestion))
+    );
+
+    expect(iterator.next().done).toBe(true);
+  });
+
+  it('should log error on failure.', () => {
+    const action = getQuestion({ difficulty: AttackStrengthType.EASY });
+    const iterator = getQuestionFetch(action);
+
+    const consoleSpy = jest.spyOn(console, 'error');
+    consoleSpy.mockReturnValue(undefined);
+
+    expect(iterator.next().value).toEqual(
+      call(
+        fetch,
+        `${BASE_QUESTION_URL}amount=1&category=21&difficulty=${AttackStrengthType.EASY}&type=multiple`
+      )
+    );
+
+    iterator.throw(new Error('api failure'));
+
+    expect(consoleSpy).toHaveBeenCalledWith('api failure');
+
+    expect(iterator.next().done).toBe(true);
   });
 });
--- src/store/game/game.saga.ts
+++ src/store/game/game.saga.ts
@@ -14,24 +14,28 @@ import { shuffleArray } from 'helper/shuffleArray';
 export function* getQuestionFetch(action: GetQuestionPayloadAction): any {
   console.log('begin');
 
-  const { difficulty } = action.payload;
-  const questionData = yield call(
-    fetch,
-    `${BASE_QUESTION_URL}amount=1&category=21&difficulty=${difficulty}&type=multiple`
-  );
-  console.log(questionData);
+  try {
+    const { difficulty } = action.payload;
+    const questionData = yield call(
+      fetch,
+      `${BASE_QUESTION_URL}amount=1&category=21&difficulty=${difficulty}&type=multiple`
+    );
+    console.log(questionData);
 
-  const res = yield questionData.json();
-  const { question, correct_answer, incorrect_answers } = res.results[0];
+    const res = yield questionData.json();
+    const { question, correct_answer, incorrect_answers } = res.results[0];
 
-  const formattedQuestion: questionData = {
-    status: QuestionStatus.IDLE,
-    text: question,
-    answer: correct_answer,
-    choices: shuffleArray([...incorrect_answers, correct_answer]),
-  };
+    const formattedQuestion: questionData = {
+      status: QuestionStatus.IDLE,
+      text: question,
+      answer: correct_answer,
+      choices: shuffleArray([...incorrect_answers, correct_answer]),
+    };
 
-  yield put(getQuestionSuccess(formattedQuestion));
+    yield put(getQuestionSuccess(formattedQuestion));
+  } catch (e: any) {
+    console.error(e.message);
+  }
 }
 
 function* questionSaga() {
