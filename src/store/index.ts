ddd05a69c11527f9ec2b1c20072aa555575fc5b4
Terrel Jones <terrel.jones@bounteous.com>
2022-08-11 10:06:42 -0700

    configured redux saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,26 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import gameReducer from 'store/game/game.slice';
+
+import { gameSagas } from 'store/game/game.saga';
+import { all } from 'redux-saga/effects';
+
+const saga = createSagaMiddleware();
 
 export const rootReducer = combineReducers({
   game: gameReducer,
 });
 
+function* sagas() {
+  yield all([gameSagas()]);
+}
+
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: [saga],
 });
+
+saga.run(sagas);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
