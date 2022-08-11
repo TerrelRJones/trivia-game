b8145e3da58612fac826e212d6946c1d62c038e7
Terrel Jones <terrel.jones@bounteous.com>
2022-08-11 10:04:17 -0700

    created game saga

---
+++ src/store/game/game.saga.ts
@@ -0,0 +1,43 @@
+import { all, call, put, takeEvery } from 'redux-saga/effects';
+import { BASE_QUESTION_URL } from 'const/Urls';
+
+import {
+  getQuestion,
+  GetQuestionPayloadAction,
+  getQuestionSuccess,
+  questionData,
+} from './game.slice';
+
+import { QuestionStatus } from 'models';
+import { shuffleArray } from 'helper/shuffleArray';
+
+export function* getQuestionFetch(action: GetQuestionPayloadAction): any {
+  console.log('begin');
+
+  const { difficulty } = action.payload;
+  const questionData = yield call(
+    fetch,
+    `${BASE_QUESTION_URL}amount=1&category=21&difficulty=${difficulty}&type=multiple`
+  );
+  console.log(questionData);
+
+  const res = yield questionData.json();
+  const { question, correct_answer, incorrect_answers } = res.results[0];
+
+  const formattedQuestion: questionData = {
+    status: QuestionStatus.IDLE,
+    text: question,
+    answer: correct_answer,
+    choices: shuffleArray([...incorrect_answers, correct_answer]),
+  };
+
+  yield put(getQuestionSuccess(formattedQuestion));
+}
+
+function* questionSaga() {
+  yield takeEvery(getQuestion.type, getQuestionFetch);
+}
+
+export function* gameSagas() {
+  yield all([questionSaga()]);
+}
