bcb3f30fba30860134d8cae7aaa082ff3bab842a
Terrel Jones <terrel.jones@bounteous.com>
2022-08-11 10:03:15 -0700

    created helper function

---
+++ src/helper/shuffleArray.ts
@@ -0,0 +1,16 @@
+export const shuffleArray = (arr: Array<string>): Array<string> => {
+  let currentIndex = arr.length;
+  let randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [arr[currentIndex], arr[randomIndex]] = [
+      arr[randomIndex],
+      arr[currentIndex],
+    ];
+  }
+
+  return arr;
+};
