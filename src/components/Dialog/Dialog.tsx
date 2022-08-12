c0018875722ed0281e392c0004f1a015f7e3c1b8
Terrel Jones <terrel.jones@bounteous.com>
2022-08-12 11:14:40 -0700

    created next button after question is answered

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,3 +1,8 @@
+import Button from 'components/Button';
+import { ButtonType } from 'components/Button/Button';
+import { DialogStageType } from 'models';
+import { gameDialogSelector } from 'store/game/game.selectors';
+import { useAppSelector } from 'store/hooks';
 import styled from 'styled-components';
 import { BodyText } from 'styles/styledElements';
 
@@ -5,11 +10,25 @@ interface DialogProps {
   message: string;
   testID?: string;
   children: React.ReactElement;
+  answered?: boolean;
 }
 
-const StyledDialogContainer = styled.div`
+const StyledDialogContainer = styled.div<Pick<DialogProps, 'answered'>>`
+  position: relative;
   display: flex;
   flex-direction: column;
+  ${({ answered }) => answered && 'margin-top: 50px;'}
+`;
+
+const NextButtonContainer = styled.div`
+  position: absolute;
+  top: -120px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 28px;
 `;
 
 const Message = styled(BodyText)`
@@ -19,6 +38,11 @@ const Message = styled(BodyText)`
   letter-spacing: -0.89px;
   line-height: 30px;
   text-align: center;
+
+  .next-message {
+    margin: 0;
+    margin-bottom: 24px;
+  }
 `;
 
 const Box = styled.div`
@@ -34,9 +58,19 @@ const Box = styled.div`
 `;
 
 export const Dialog = ({ testID, message, children }: DialogProps) => {
+  const dialogStage = useAppSelector(gameDialogSelector);
+  const answeredStage = dialogStage === DialogStageType.ANSWERED;
+
   return (
-    <StyledDialogContainer data-testid={testID}>
-      <Message data-testid="title">{message}</Message>
+    <StyledDialogContainer answered={answeredStage} data-testid={testID}>
+      {answeredStage ? (
+        <NextButtonContainer>
+          <Message className="next-message">Correct!</Message>
+          <Button buttonType={ButtonType.NEXT}>Next</Button>
+        </NextButtonContainer>
+      ) : (
+        <Message data-testid="title">{message}</Message>
+      )}
       <Box>{children}</Box>
     </StyledDialogContainer>
   );
