f9340a28d9d8e5de13cbfa6a380092a23f1fe3e5
Terrel Jones <terrel.jones@bounteous.com>
2022-08-15 10:41:14 -0700

    refactored test for more predictable outcome

--- src/helper/shuffleArray.test.ts
+++ src/helper/shuffleArray.test.ts
@@ -2,9 +2,19 @@ import { shuffleArray } from 'helper/shuffleArray';
 
 describe('shuffleArray function', () => {
   it('should return array in random arragment.', () => {
+    const randomSpy = jest.spyOn(Math, 'random');
+    randomSpy
+      .mockReturnValueOnce(0.25)
+      .mockReturnValueOnce(0.75)
+      .mockReturnValueOnce(0.1)
+      .mockReturnValueOnce(0.9);
+
     const arr1 = ['Hi', 'I', 'am', 'Terrel'];
     const shuffledArr = shuffleArray(arr1);
 
-    expect(shuffledArr).not.toBe(['Hi', 'I', 'am', 'Terrel']);
+    expect(shuffledArr).toEqual(['Terrel', 'Hi', 'am', 'I']);
+    expect(randomSpy).toHaveBeenCalledTimes(arr1.length);
+
+    randomSpy.mockRestore();
   });
 });
