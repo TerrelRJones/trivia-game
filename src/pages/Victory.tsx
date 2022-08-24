db022eb9acb08ee3b64bb01529edf9a5a1e13a67
Terrel Jones <terrel.jones@bounteous.com>
2022-08-24 09:20:06 -0700

    added test case

--- src/pages/Victory.tsx
+++ src/pages/Victory.tsx
@@ -16,59 +16,6 @@ import { useNavigate } from 'react-router-dom';
 import { useOpponentDetails } from 'store/opponent/opponent.hooks';
 import { useGameReset } from 'store/game/game.hooks';
 
-const DefeatPageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 100vh;
-`;
-
-const Header = styled.h1`
-  font-family: Impact;
-  font-size: 99px;
-  letter-spacing: 0;
-  line-height: 121px;
-  text-transform: uppercase;
-  margin: 0;
-
-  @media (max-width: 400px) {
-    font-size: 80px;
-    line-height: 80px;
-  }
-`;
-
-const HeroContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const DefeatText = styled(BodyText)`
-  font-size: 30px;
-  margin: 0;
-`;
-
-const ButtonContainer = styled.div`
-  margin-top: 40px;
-`;
-
-const AvatarContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  max-width: 477px;
-  max-height: 500px;
-  margin-top: 104px;
-`;
-
-const Avatar = styled.img`
-  z-index: 2;
-  margin-left: 25%;
-  max-height: 500px;
-  height: 100%;
-`;
-
 const ShadowGradient = styled.img`
   position: absolute;
   left: 50%;
