ae4611a6388b94d18eb27bb798a89a09aa5431ca
Terrel Jones <terrel.jones@bounteous.com>
2022-08-24 09:22:11 -0700

    added test

--- src/pages/_test_/Defeat.test.tsx
+++ src/pages/_test_/Defeat.test.tsx
@@ -1,6 +1,21 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithProviders } from 'testHelpers';
 import Defeat from '../Defeat';
+import { useNavigate } from 'react-router-dom';
+
+// jest mock runs before EVERYTHING!!!!
+jest.mock('react-router-dom', () => {
+  const mockNavigate = jest.fn();
+
+  const module = jest.requireActual('react-router-dom');
+  return {
+    __esModule: true,
+    ...module,
+    useNavigate: () => {
+      return mockNavigate;
+    },
+  };
+});
 
 describe('Defeat Page', () => {
   it('should render all Defeat page ui to screen. Header that displays Game Over, play again btn, and defeated Fox Avatar.', () => {
@@ -8,9 +23,18 @@ describe('Defeat Page', () => {
 
     expect(screen.getByTestId('header')).toHaveTextContent('GAME OVER');
     expect(screen.getByTestId('play-again-btn')).toBeInTheDocument();
-    expect(screen.getByTestId('avatar')).toHaveAttribute(
-      'src',
-      'fox-knight_defeated.svg'
-    );
+  });
+
+  it('should call naviagte() after clicking play again button.', () => {
+    renderWithProviders(<Defeat />);
+
+    const mockNavigateFN = useNavigate();
+    expect(mockNavigateFN).not.toHaveBeenCalled();
+
+    const playAgainButton = screen.getByTestId('play-again-btn');
+    fireEvent.click(playAgainButton);
+
+    expect(mockNavigateFN).toHaveBeenLastCalledWith('/');
+    expect(mockNavigateFN).toHaveBeenCalledTimes(1);
   });
 });
