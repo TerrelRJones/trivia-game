44c8a73f82d513171084734dd8efb2fc18a348b7
Terrel Jones <terrel.jones@bounteous.com>
2022-08-29 08:00:37 -0700

    fixed test

--- src/pages/_test_/Game.test.tsx
+++ src/pages/_test_/Game.test.tsx
@@ -7,7 +7,6 @@ describe('Game Page', () => {
   it('should render all game ui to screen. Both healthBars, Round, Players, and Dialog Box.', () => {
     renderWithProviders(<Game testID="game-ui" />);
 
-    expect(screen.getByTestId('game-ui')).toBeInTheDocument();
     expect(screen.getByTestId('health-bar-1')).toBeInTheDocument();
     expect(screen.getByTestId('health-bar-2')).toBeInTheDocument();
     expect(screen.getByTestId('round')).toBeInTheDocument();
